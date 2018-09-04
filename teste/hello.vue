6931570b08bdc1d5609520e63ab7a3ede25f3e40
CleverMatias <clevermatias@hotmail.com>
2018-09-04 15:00:49 -0300

    add .vue test file

---
+++ teste/hello.vue
@@ -0,0 +1,17 @@
+<template>
+	<p>{{hello}}</p>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				hello: 'Hello from vue!'
+			}
+		}
+	}
+</script>
+<style scoped>
+	p{
+		color: blue;
+	}
+</style>
